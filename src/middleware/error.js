b3218c846084830715448d678e8571f94e86e6b4
TheSaviourEking <saviour.eking@outlook.com>
2024-03-28 02:16:33 +0100

    chore: add basic error handlers
    would refactor later

---
+++ src/middleware/error.js
@@ -0,0 +1,36 @@
+const isProduction = process.env.NODE_ENV === 'production';
+const { ValidationError } = require('sequelize');
+// error handlers
+// Resource Not Found Error-Handler
+const resourceNotFound = (_req, res, next) => {
+    const err = new Error('The requested resource couldn\'t be found.');
+    err.title = 'Resource Not Found';
+    err.errors = ['The requested resource couldn\'t be found.'];
+    err.status = 404;
+    next(err);
+}
+
+// Sequelize Error-Handler
+const sequelizeError = (err, _req, _res, next) => {
+    // check if error is a Sequelize error
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map(e => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+}
+
+// Error formatter
+const errorFormatter = (err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+        handler: 'errorFormatter'
+    });
+};
+
+module.exports = { resourceNotFound, sequelizeError, errorFormatter }
