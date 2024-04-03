8d7057bcfb2395577a6ae1c4ebcdfdce8d81a420
TheSaviourEking <saviour.eking@outlook.com>
2024-04-03 04:07:38 +0100

    chore: redesign the db to make do with just important user information

--- src/database/migrations/20240324062620-create-user.js
+++ src/database/migrations/20240324062620-create-user.js
@@ -26,7 +26,7 @@ module.exports = {
       },
       password: {
         type: Sequelize.STRING,
-        allowNull: false
+        // allowNull: false
       },
       createdAt: {
         allowNull: false,
