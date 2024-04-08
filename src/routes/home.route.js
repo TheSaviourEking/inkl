47fcdea82d162463d02987f663abf3b1169377b8
TheSaviourEking <saviour.eking@outlook.com>
2024-04-08 03:32:02 +0100

    refactor: clean up router

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -6,5 +6,6 @@ const { renderHomePage, renderLoginPage, renderSignupPage } = require('../contro
 router.get('/login', renderLoginPage);
 router.get('/signup', renderSignupPage);
 router.get('/', renderHomePage);
+// router.delete('/', (req, res) => res.json({ message: "here" }))
 
 module.exports = router;
