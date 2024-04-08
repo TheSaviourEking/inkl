9e1217b40c32c2d36516ff7bfbd06a0e0acaa448
TheSaviourEking <saviour.eking@outlook.com>
2024-04-09 00:49:20 +0100

    refactor: DRYup code

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,22 +1,38 @@
-const getGoogleOauthUrl = require('../utils/getGoogleAuth.js');
+const getGoogleOauthUrl = require('../utils/auth/google/getGoogleAuth.js');
+const path = require('path');
 
 const renderHomePage = (req, res) => {
     // res.json('here')
+    const cards = [
+        "Never miss what's important",
+        "Save Time, Read More",
+        "Stay Informed",
+        "Read with confidence"
+    ];
+    const newsCategories = ['tech', 'crypto', 'ai', 'jobs'];
+    const news = ['']
+    // req.user = true;
     const csrfToken = req.csrfToken();
     res.cookie('XSRF-TOKEN', csrfToken);
-    res.render('pages/home')
+    res.sendFile(path.resolve(__dirname, '..', 'views', 'pages', 'home.html'))
 }
 
 const renderLoginPage = (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie('XSRF-TOKEN', csrfToken);
-    res.render('pages/login', { _csrf: csrfToken, googleAuth: getGoogleOauthUrl() });
+    // res.render('pages/login', {
+    //     _csrf: csrfToken,
+    //     googleAuth: getGoogleOauthUrl(),
+    //     // microsoftAuth: getMicrosoftOauthUrl(),
+    // });
+    res.sendFile(path.resolve(__dirname, '..', 'views', 'pages', 'login.html'))
 }
 
 const renderSignupPage = (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie('XSRF-TOKEN', csrfToken);
-    res.render('pages/signup', { _csrf: csrfToken })
+    // res.render('pages/signup', { _csrf: csrfToken })
+    res.sendFile(path.resolve(__dirname, '..', 'views', 'pages', 'signup.html'))
 }
 
 module.exports = { renderHomePage, renderLoginPage, renderSignupPage };
