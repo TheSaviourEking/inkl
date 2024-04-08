6758547fd3c2ae2c4cb026bbedc8ed69194c7c7f
TheSaviourEking <saviour.eking@outlook.com>
2024-04-08 03:29:52 +0100

    refactor: move token function to the session controller

--- src/routes/api/session.js
+++ src/routes/api/session.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { login, logout, restoreSession } = require('../../controllers/api/session.controller.js');
-const { restoreUser } = require('../../utils/token.js');
+const { login, logout, restoreSession, refreshTokens } = require('../../controllers/api/session.controller.js');
+const { restoreUser, authenticateTokenCookie } = require('../../utils/token.js');
+
+router.post('/token', refreshTokens)
 
 router.post('/', login);
 router.delete('/', logout);
-router.get('/', restoreUser, restoreSession)
+// router.get('/', restoreUser, restoreSession)
+router.get('/', authenticateTokenCookie, restoreSession)
 
 module.exports = router;
