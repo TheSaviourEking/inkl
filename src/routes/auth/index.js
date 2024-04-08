c84ed7c0ca374b6d11a6fd7d18420a935e647903
TheSaviourEking <saviour.eking@outlook.com>
2024-04-09 00:51:49 +0100

    feat: attempt microsoft oauth2 auth

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/google', require('./google.js'));
-// router.use('/google', require('./google.js'));
+router.use('/microsoft', require('./microsoft.js'));
 // router.use('/google', require('./google.js'));
 router.use('/error', require('./error.js'));
 
