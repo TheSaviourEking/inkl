62e2318e9e64ab43dd2524a9fe0d58c49bd3ff3a
TheSaviourEking <saviour.eking@outlook.com>
2024-04-09 00:52:55 +0100

    chore: refactor: clean up codebase

---
+++ src/utils/auth/microsoft/auth.js
@@ -0,0 +1,7 @@
+const getMicrosoftOauthTokens = async ({ code }) => {
+    // const url = 'https://login.microsoftonline.com/common/',
+    const url = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize';
+    const values = {}
+}
+
+module.exports = { getMicrosoftOauthTokens }
