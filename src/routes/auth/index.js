50d27c5428a166010be6f354bb1f0d31f3424630
TheSaviourEking <saviour.eking@outlook.com>
2024-04-08 03:30:38 +0100

    chore: add error route handler

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use('/google', require('./google.js'));
 // router.use('/google', require('./google.js'));
 // router.use('/google', require('./google.js'));
+router.use('/error', require('./error.js'));
 
 module.exports = router;
