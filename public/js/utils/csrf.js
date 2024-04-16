91b4ed802ee4008c788d6c8e009825b025eb4d32
TheSaviourEking <saviour.eking@outlook.com>
2024-04-16 05:34:17 +0100

    refactor: update getToken function to be adaptable to all tokens

--- public/js/utils/csrf.js
+++ public/js/utils/csrf.js
@@ -1,15 +1,23 @@
-function getCSRFTokenCookie() {
-    // CSRF Protection:
+// function getCSRFTokenCookie() {
+//     // CSRF Protection:
+//     const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
+//         const [name, value] = cookie.split('=');
+//         acc[name] = value;
+//         return acc;
+//     }, {});
+//     const csrfToken = cookies['XSRF-TOKEN'];
+//     return csrfToken;
+// }
+function getToken(token) {
     const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
         const [name, value] = cookie.split('=');
         acc[name] = value;
         return acc;
     }, {});
-    const csrfToken = cookies['XSRF-TOKEN'];
-    return csrfToken;
+    return cookies[token];
 }
 window.onload = () => {
     const csrfTokenInput = document.querySelector('#_csrf');
-    console.log(csrfTokenInput)
-    csrfTokenInput.value = getCSRFTokenCookie();
+    // console.log(csrfTokenInput)
+    // csrfTokenInput.value = getToken('XSRF-TOKEN');
 }
